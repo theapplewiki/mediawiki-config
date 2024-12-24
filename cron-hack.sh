64aacf64b45932614e9e0954a9c133721b7fb603
Adam Demasi <adam@hbang.com.au>
2024-12-25 03:25:37 +1030

    I'm tired

--- cron-hack.sh
+++ cron-hack.sh
@@ -7,5 +7,5 @@ source .env
 for i in $WIKIS; do
 	# Hack workaround for RecentChanges not being updated automatically for some reason
 	nice docker compose exec mediawiki \
-		maintenance/run --wiki $1 rebuildrecentchanges --quiet
+		maintenance/run --wiki $i rebuildrecentchanges --quiet
 done
