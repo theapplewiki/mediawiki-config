f7d6ad4a78e849496b5a67695382b9a2b8143f83
Adam Demasi <adam@hbang.com.au>
2025-01-26 02:05:18 +1030

    SORBS is dead

--- LocalSettings.php
+++ LocalSettings.php
@@ -414,7 +414,7 @@ $wgHCaptchaSendRemoteIP = true;
 
 // DNS denylist
 $wgEnableDnsBlacklist = $wikiID != 'testwiki';
-$wgDnsBlacklistUrls   = ['xbl.spamhaus.org.', 'spam.dnsbl.sorbs.net.', 'rbl.dnsbl.im.', 'noptr.spamrats.com.', 'all.s5h.net.', 'z.mailspike.net.'];
+$wgDnsBlacklistUrls   = ['xbl.spamhaus.org.', 'rbl.dnsbl.im.', 'noptr.spamrats.com.', 'all.s5h.net.', 'z.mailspike.net.'];
 $wgSuspiciousIpExpiry = 24 * 60 * 60; // 1 day
 
 // User CSS/JS
